741159851533440deda67fba43192537f81f0036
Helena Pedro <mbeua94@gmail.com>
2024-11-21 18:48:04 +0100

    new commit

--- pages/home.py
+++ pages/home.py
@@ -8,36 +8,36 @@ from utils.dropdown import dropdown_menu
 def create_home_page():
     return dbc.Container([
         html.Div(children=[
-            html.H1('Launch Records Dashboard',
-                    style={'textAlign': 'center', 'padding': '20px', 'color': '#4CAF50'}),
+            html.H1('Launch Records Dashboard', className='text-center mb-4', style={'color': '#4CAF50'}),
         ]),
-        html.Br(),
         dbc.Row([
             dbc.Col([
                 dcc.Dropdown(id='site-dropdown',
                             options=dropdown_menu(),
                             value='ALL',
-                            placeholder="State",
-                            searchable=True),
-            ],  width=6)
-        ]),
-        html.Br(),
+                            placeholder="Select State",
+                            searchable=True,
+                            className='form-control'),
+            ], xs=12, sm=12, md=6, lg=6, xl=6)
+        ], className='mb-4'),
         dbc.Row([
             dbc.Col([
                 dcc.Graph(id='success-pie-chart'),
-            ], width=6),
+            ], xs=12, sm=12, md=6, lg=6, xl=6, className='mb-4'),
             dbc.Col([
                 dcc.Graph(id='success-payload-scatter-chart'),
-            ], width=6),
+            ], xs=12, sm=12, md=6, lg=6, xl=6, className='mb-4'),
         ]),
-        html.P("Payload range (Kg):"),
+        html.P("Payload range (Kg):", className='mb-2'),
         dcc.RangeSlider(
             id='payload-slider',
             min=0, max=10000, step=1000,
             marks={0: '0', 1000: '1000'},
-            value=get_payload_range()
+            value=get_payload_range(),
+            className='mb-4'
         ),
-    ], className="container mt-5",)
+    ], fluid=True, className="mt-5")
+
 
 @callback(Output(component_id='success-pie-chart', component_property='figure'),
               Input(component_id='site-dropdown', component_property='value'))
