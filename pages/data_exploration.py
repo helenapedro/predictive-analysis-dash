0f91d34f63abcdc403d8a1b6e1ab08c90aff1803
Helena Pedro <mbeua94@gmail.com>
2024-11-21 17:28:11 +0100

    new commit

--- pages/data_exploration.py
+++ pages/data_exploration.py
@@ -58,86 +58,235 @@ cores_df = cores_df.rename(columns={
 })
 
 def create_exploration_page():
-     return (
-          html.H1('Data Exploration', style={'textAlign': 'center'}),
-          dcc.Tabs([
-               dcc.Tab(label='Rockets', children=[
-                    dash_table.DataTable(
-                         id='rockets-table',
-                         columns=[{"name": i, "id": i} for i in rockets_df.columns],
-                         data=rockets_df.to_dict('records'),
-                         style_table={'overflowX': 'scroll'},
-                         style_cell={'textAlign': 'left'},
-                    )
-               ]),
-               dcc.Tab(label='Launchpads', children=[
-                    dash_table.DataTable(
-                         id='launchpads-table',
-                         columns=[{"name": i, "id": i} for i in launchpads_df.columns],
-                         data=launchpads_df.to_dict('records'),
-                         style_table={'overflowX': 'scroll'},
-                         style_cell={'textAlign': 'left'},
-                    )
-               ]),
-               dcc.Tab(label='Payloads', children=[
-                    dash_table.DataTable(
-                         id='payloads-table',
-                         columns=[{"name": i, "id": i} for i in payloads_df.columns],
-                         data=payloads_df.to_dict('records'),
-                         style_table={'overflowX': 'scroll'},
-                         style_cell={'textAlign': 'left'},
-                    )
-               ]),
-               dcc.Tab(label='Cores', children=[
-                    dash_table.DataTable(
-                         id='cores-table',
-                         columns=[{"name": i, "id": i} for i in cores_df.columns],
-                         data=cores_df.to_dict('records'),
-                         style_table={'overflowX': 'scroll'},
-                         style_cell={'textAlign': 'left'},
-                    )
-               ]),
-               dcc.Tab(label='Payload Mass Distribution', children=[
-                    dcc.Graph(
-                         id='payload-mass-distribution',
-                         figure={
-                              'data': [
-                                   {
-                                        'x': payloads_df['name'],
-                                        'y': payloads_df['mass_kg'],
-                                        'type': 'bar',
-                                        'marker': {'color': 'blue'},  # Add color
-                                        'name': 'Mass (kg)',
-                                   },
-                              ],
-                              'layout': {
-                                   'title': {'text': 'Payload Mass Distribution', 'x': 0.5},  # Center title
-                                   'xaxis': {'title': 'Payload Name', 'automargin': True},  # Auto margins
-                                   'yaxis': {'title': 'Mass (kg)', 'automargin': True},
-                                   'template': 'plotly_dark',  # Dark theme
-                              }
-                         }
-                    )
-               ]),
-               dcc.Tab(label='Core Reuse Count', children=[
-                    dcc.Graph(
-                         id='core-reuse-count',
-                         figure={
-                              'data': [
-                              {
-                                   'x': cores_df['Core Serial Number'],
-                                   'y': cores_df['Times Reused'],
-                                   'type': 'bar',
-                                   'name': 'Times Reused',
+    return html.Div(
+        [
+            # Page Title
+            html.H1('Data Exploration', style={'textAlign': 'center', 'padding': '20px', 'color': '#4CAF50'}),
+
+            # Information Section
+            html.Div(
+                [
+                    html.Button(
+                        "Show/Hide Snippet Code", 
+                        id="toggle-button", 
+                        n_clicks=0,
+                        className="btn btn-primary mb-3"
+                    ),
+                    html.Div(
+                        [
+                            html.H3(
+                                "This dataset was gathered using a GET request from the SpaceX REST API. "
+                                "To explore how this data was collected, click the button above to view the snippet code.",
+                                className="text-start"
+                            ),
+                            html.Pre(
+                                """
+import requests, import pandas as pd
+                                             
+def fetch_data_from_api(endpoint):
+     base_url = 'https://api.spacexdata.com/v4/'
+     url = f"{base_url}{endpoint}"
+     try:
+          response = requests.get(url)
+          response.raise_for_status()
+          data = response.json()
+          return data
+     except requests.exceptions.RequestException as e:
+          ...
+
+def fetch_rockets_data(): return fetch_data_from_api('rockets')
+def fetch_launchpads_data(): return fetch_data_from_api('launchpads')
+def fetch_payloads_data(): return fetch_data_from_api('payloads')
+def fetch_cores_data(): return fetch_data_from_api('cores')
+                                """,
+                                id="summary-content",
+                                style={
+                                    'backgroundColor': '#f4f4f4',
+                                    'padding': '10px',
+                                    'borderRadius': '5px',
+                                    'whiteSpace': 'pre-wrap',
+                                    'overflowX': 'scroll',
+                                },
+                                className="pre-scrollable"
+                            ),
+                        ],
+                        style={'marginBottom': '20px'},
+                        className="card p-3"
+                    ),
+                ],
+                className="container mt-5",
+            ),
+
+            # Tabs Section
+            dcc.Tabs(
+                [
+                    dcc.Tab(label='Rockets', children=[
+                         html.Div(
+                              dash_table.DataTable(
+                                  id='rockets-table',
+                              columns=[{"name": i, "id": i} for i in rockets_df.columns],
+                              data=rockets_df.to_dict('records'),
+                              style_table={'overflowX': 'scroll'},
+                              style_cell={
+                                   'textAlign': 'left',
+                                   'minWidth': '150px',
+                                   'width': 'auto',
+                                   'maxWidth': '300px',
+                                   'whiteSpace': 'normal',
+                                   'padding': '10px', 
                               },
-                              ],
-                              'layout': {
-                              'title': 'Core Reuse Count',
-                              'xaxis': {'title': 'Core Serial Number'},
-                              'yaxis': {'title': 'Times Reused'},
+                              style_header={
+                                   'textAlign': 'center',
+                                   'backgroundColor': '#f1f1f1',  
+                                   'fontWeight': 'bold', 
+                                   'minWidth': '150px',  
+                                   'width': 'auto',
+                                   'maxWidth': '300px',  
+                                   'padding': '10px',  
                               }
-                         }
-                    )
-               ]),
-          ])
-     )
+                            ),
+                            className="card p-3 mb-4"
+                        )
+                    ], className="nav-item card p-3"),
+
+                    dcc.Tab(label='Launchpads', children=[
+                        html.Div(
+                            dash_table.DataTable(
+                                id='launchpads-table',
+                                columns=[{"name": i, "id": i} for i in launchpads_df.columns],
+                                data=launchpads_df.to_dict('records'),
+                                style_table={'overflowX': 'scroll'},
+                                style_cell={
+                                   'textAlign': 'left',
+                                   'minWidth': '150px',
+                                   'width': 'auto',
+                                   'maxWidth': '300px',
+                                   'whiteSpace': 'normal',
+                                   'padding': '10px', 
+                              },
+                              style_header={
+                                   'textAlign': 'center',
+                                   'backgroundColor': '#f1f1f1',  
+                                   'fontWeight': 'bold', 
+                                   'minWidth': '150px',  
+                                   'width': 'auto',
+                                   'maxWidth': '300px',  
+                                   'padding': '10px',  
+                              }
+                            ),
+                            className="card p-3 mb-4"
+                        )
+                    ], className="nav-item card p-3"),
+
+                    dcc.Tab(label='Payloads', children=[
+                        html.Div(
+                            dash_table.DataTable(
+                                id='payloads-table',
+                                columns=[{"name": i, "id": i} for i in payloads_df.columns],
+                                data=payloads_df.to_dict('records'),
+                                style_table={'overflowX': 'scroll'},
+                                style_cell={
+                                   'textAlign': 'left',
+                                   'minWidth': '150px',
+                                   'width': 'auto',
+                                   'maxWidth': '300px',
+                                   'whiteSpace': 'normal',
+                                   'padding': '10px', 
+                              },
+                              style_header={
+                                   'textAlign': 'center',
+                                   'backgroundColor': '#f1f1f1',  
+                                   'fontWeight': 'bold', 
+                                   'minWidth': '150px',  
+                                   'width': 'auto',
+                                   'maxWidth': '300px',  
+                                   'padding': '10px',  
+                              }
+                            ),
+                            className="card p-3 mb-4"
+                        )
+                    ], className="nav-item card p-3"),
+
+                    dcc.Tab(label='Cores', children=[
+                        html.Div(
+                            dash_table.DataTable(
+                                id='cores-table',
+                                columns=[{"name": i, "id": i} for i in cores_df.columns],
+                                data=cores_df.to_dict('records'),
+                                style_table={'overflowX': 'scroll'},
+                                style_cell={
+                                   'textAlign': 'left',
+                                   'minWidth': '150px',
+                                   'width': 'auto',
+                                   'maxWidth': '300px',
+                                   'whiteSpace': 'normal',
+                                   'padding': '10px', 
+                              },
+                              style_header={
+                                   'textAlign': 'center',
+                                   'backgroundColor': '#f1f1f1',  
+                                   'fontWeight': 'bold', 
+                                   'minWidth': '150px',  
+                                   'width': 'auto',
+                                   'maxWidth': '300px',  
+                                   'padding': '10px',  
+                              }
+                            ),
+                            className="card p-3 mb-4"
+                        )
+                    ], className="nav-item card p-3"),
+
+                    dcc.Tab(label='Payload Mass Distribution', children=[
+                        html.Div(
+                            dcc.Graph(
+                                id='payload-mass-distribution',
+                                figure={
+                                    'data': [
+                                        {
+                                            'x': payloads_df['name'],
+                                            'y': payloads_df['mass_kg'],
+                                            'type': 'bar',
+                                            'marker': {'color': 'blue'},
+                                            'name': 'Mass (kg)',
+                                        },
+                                    ],
+                                    'layout': {
+                                        'title': {'text': 'Payload Mass Distribution', 'x': 0.5},
+                                        'xaxis': {'title': 'Payload Name', 'automargin': True},
+                                        'yaxis': {'title': 'Mass (kg)', 'automargin': True},
+                                        'template': 'plotly_dark',
+                                    }
+                                }
+                            ),
+                            className="card p-3 mb-4"
+                        )
+                    ], className="nav-item card p-3"),
+
+                    dcc.Tab(label='Core Reuse Count', children=[
+                        html.Div(
+                            dcc.Graph(
+                                id='core-reuse-count',
+                                figure={
+                                    'data': [
+                                        {
+                                            'x': cores_df['Core Serial Number'],
+                                            'y': cores_df['Times Reused'],
+                                            'type': 'bar',
+                                            'name': 'Times Reused',
+                                        },
+                                    ],
+                                    'layout': {
+                                        'title': 'Core Reuse Count',
+                                        'xaxis': {'title': 'Core Serial Number'},
+                                        'yaxis': {'title': 'Times Reused'},
+                                    }
+                                }
+                            ),
+                            className="card p-3 mb-4"
+                        )
+                    ], className="nav-item card p-3"),
+                ],
+            ),
+        ],
+        className="container mt-5",
+    )
