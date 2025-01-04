da2a8a43749bbfe280dbbe5bb08f18f9dcf4b62f
Helena Pedro <mbeua94@gmail.com>
2025-01-05 00:43:17 +0100

    new commit

--- data/row_data.py
+++ data/row_data.py
@@ -60,6 +60,43 @@ def fetch_initial_data_layout():
                             ],
                             className="hero-section"
                     ),
+
+                    dbc.ListGroup(
+                        [
+                            dbc.ListGroupItem("Rocket: Retrieve the booster name."),
+                            dbc.ListGroupItem(
+                                html.Div(
+                                    [
+                                    html.Span("Payload: ", ),
+                                    html.Span("Obtain the mass of the payload and the orbit it will enter.")
+                                    ]
+                                )
+                            ),
+                            dbc.ListGroupItem(
+                                html.Div(
+                                    [
+                                    html.Span("Launchpad: ", ),
+                                    html.Span("Identify the name of the launch site, as well as its longitude and latitude.")
+                                    ]
+                                )
+                            ),
+                            dbc.ListGroupItem(
+                                html.Div(
+                                    [
+                                    html.Span("Cores: ", ),
+                                    html.Span(
+                                            """
+                                                Cores: Gather detailed information including the landing outcome, landing type, 
+                                                number of flights for that core, usage of grid fins, whether the core is reused, 
+                                                presence of legs, the landing pad used, the block version, reuse count, and the 
+                                                serial number of the core.
+                                            """,
+                                    )
+                                    ]
+                                )
+                            ),
+                        ]
+                    ),
                 ]
             ),
             
